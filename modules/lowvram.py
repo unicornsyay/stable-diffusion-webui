ec01dc05c218a2039538cc926c661c1a3009285d
unicornsyay <30913640+unicornsyay@users.noreply.github.com>
2023-05-18 16:27:27 -0400

    Add files via upload
    Adds Memory Checks and if using --lowvram or --medvram will process with CPU if you have low VRAM. 
    Adds "threshold" which you can edit to set the videoram threshold for when you switch from video to CPU. 
    Performs a basic memory check for torch. 
    Note: not to be used when training models, because it is really slow!

--- modules/lowvram.py
+++ modules/lowvram.py
@@ -1,8 +1,39 @@
 import torch
 from modules import devices
+## if you want to reduce memory requirements, use model simplification, quantization, gradient checkpointing, mixed precision training, or working with smaller batch sizes
 
 module_in_gpu = None
 cpu = torch.device("cpu")
+# Set the GPU memory threshold in bytes
+threshold = 0.80 * torch.cuda.get_device_properties(0).total_memory  # 80% of total GPU memory, provides a 20% buffer
+# Perform GPU computations
+tensor_gpu = torch.randn(10, 3, 1600, 1600, device='cuda')
+#10 = batch_size = number of images being processed // may need to refine this. 
+#3 = RGB colors used by MOST models minus scientific ones which may have more data - but this should be kept as '3'
+#width - the width of the  images
+#height = the height of the image.
+## This is a basic memory check and won't handle spikes during training
+
+def check_memory_and_allocate(shape):
+    # Get current memory usage
+    current_memory = torch.cuda.memory_allocated()
+    # Get total GPU memory
+    total_memory = torch.cuda.get_device_properties(0).total_memory
+    # Calculate the size of the new tensor
+    new_tensor_memory = torch.Tensor(*shape).numel() * 4  # 4 bytes per float32
+    # Check if there is enough memory left for the new tensor
+    if current_memory + new_tensor_memory > total_memory * 0.9:  # leave 10% buffer
+        print("Insufficient memory to create new tensor. Reducing batch size or model size might be necessary.")
+        #return torch.randn(*shape, device='cuda') #can uncomment if you want to use CPU when getting warning
+    else:
+        return torch.randn(*shape, device='cuda')
+
+if torch.cuda.memory_allocated() > threshold:
+    print("Switching to CPU...")
+    tensor_cpu = tensor_gpu.to('cpu')
+else:
+    print("Continuing with GPU computations...")
+    result_gpu = tensor_gpu * 2
 
 
 def send_everything_to_cpu():
@@ -22,9 +53,14 @@ def setup_for_low_vram(sd_model, use_medvram):
         we add this as forward_pre_hook to a lot of modules and this way all but one of them will
         be in CPU
         """
-        global module_in_gpu
-
-        module = parents.get(module, module)
+        global module_in_gpu  
+        # Monitor GPU memory usage and switch to CPU if threshold is exceeded
+        threshold = torch.cuda.max_memory_allocated() *0.85 #15% buffer - not to be used when training models
+        if threshold > 0:
+            if torch.cuda.memory_allocated() > threshold:
+                print("Switching to CPU...")
+                send_everything_to_cpu()
+                module = parents.get(module, module)
 
         if module_in_gpu == module:
             return
@@ -44,11 +80,11 @@ def setup_for_low_vram(sd_model, use_medvram):
     first_stage_model_decode = sd_model.first_stage_model.decode
 
     def first_stage_model_encode_wrap(x):
-        send_me_to_gpu(first_stage_model, None)
+        send_me_to_gpu(first_stage_model, None)       
         return first_stage_model_encode(x)
 
     def first_stage_model_decode_wrap(z):
-        send_me_to_gpu(first_stage_model, None)
+        send_me_to_gpu(first_stage_model, None)          
         return first_stage_model_decode(z)
 
     # for SD1, cond_stage_model is CLIP and its NN is in the tranformer frield, but for SD2, it's open clip, and it's in model field
@@ -79,6 +115,9 @@ def setup_for_low_vram(sd_model, use_medvram):
 
     if use_medvram:
         sd_model.model.register_forward_pre_hook(send_me_to_gpu)
+        if torch.cuda.memory_allocated() > threshold:
+            print("Switching to CPU...")
+            send_everything_to_cpu()
     else:
         diff_model = sd_model.model.diffusion_model
 
@@ -96,3 +135,8 @@ def setup_for_low_vram(sd_model, use_medvram):
         diff_model.middle_block.register_forward_pre_hook(send_me_to_gpu)
         for block in diff_model.output_blocks:
             block.register_forward_pre_hook(send_me_to_gpu)
+        # Monitor GPU memory usage and switch to CPU if threshold is exceeded
+            if torch.cuda.memory_allocated() > threshold:
+                print("Switching to CPU...")
+                send_everything_to_cpu()
+
